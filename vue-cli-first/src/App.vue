ebc59ca8d41aff7fda4585877f9a88cff708c913
DenuccioDiego <diegodenuccio95@gmail.com>
2021-11-10 16:55:09 +0100

    Esercizio completo con bonus

---
+++ vue-cli-first/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>La mia prima app con Vue CLI!</h1>
+    
+    <jumbotron/>
+  </div>
+</template>
+
+<script>
+import jumbotron from"./components/jumbotron.vue"
+
+export default {
+  name: 'App',
+  components: {
+    jumbotron
+  }
+}
+</script>
+
+
+
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli-first/src/components/jumbotron.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <div class="image_jumbo">
+      <h2>Bonus completo</h2>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "jumbotron",
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .image_jumbo{
+    background-image: url("../assets/Giornata-Mondiale-della-Terra-2.jpg");
+    height: 80vh;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+</style>
